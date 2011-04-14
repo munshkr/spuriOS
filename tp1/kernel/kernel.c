a8f509d878c2db8e98b9cccc30b7ac72de83e91d
munshkr <munshkr@gmail.com>
2011-04-14 00:51:32 -0300

    Pruebas VGA

--- tp1/kernel/kernel.c
+++ tp1/kernel/kernel.c
@@ -13,14 +13,24 @@ extern void* _end;
 /* Entry-point del modo protegido luego de cargar los registros de
  * segmento y armar un stack */
 void kernel_init(void) {
-	cli();
+	gdt_init();
 
-	//gdt_init();	
+	vga_init();
 
-	vga_putchar('a');
-	//vga_clear();
-	//vga_printf("Hello World!\n");
-	//printf("Fruta \\c0A %d %s", 123, "hola");
+	vga_printf("Hello World!\n");
+	vga_printf("Fruta \\c0a%d %s\\c07, pero todo bien ahora\n", 123, "hola");
+
+	int i;
+	vga_attr.fld.light = TRUE;
+	for (i = 0; i < 8; ++i) {
+		vga_attr.fld.forecolor = i;
+		if (i % 2 == 0) vga_attr.fld.backcolor = i;
+		vga_printf("shake it\n");
+	}
+
+	// 80 is an invalid column, so it raises an assertion
+	vga_set_pos(80, 12);
+	vga_putchar('F');
 
 	return;
 }
