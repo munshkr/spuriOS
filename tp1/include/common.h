26b472324447e22580e44212e68a44e4ddad234e
munshkr <munshkr@gmail.com>
2011-04-27 20:43:40 -0300

    Add (missing?) variable argument definitions

--- tp1/include/common.h
+++ tp1/include/common.h
@@ -9,4 +9,12 @@
 void memcpy(void* src, void* dst, size_t size);
 void memset(void* addr, int value, size_t size);
 
+// Use GCC built-in functionality for variable arguments
+#define va_start(v,l) __builtin_va_start(v,l)
+#define va_arg(v,l)   __builtin_va_arg(v,l)
+#define va_end(v)     __builtin_va_end(v)
+#define va_copy(d,s)  __builtin_va_copy(d,s)
+
+typedef __builtin_va_list va_list;
+
 #endif // __COMMON_H__
