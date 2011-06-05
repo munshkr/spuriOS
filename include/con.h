8bedf64780800ea929845afbb2c41421394f507a
Damián Emiliano Silvani <munshkr@gmail.com>
2011-06-05 15:44:35 -0300

    con: Run screen saver when all consoles are closed

--- include/con.h
+++ include/con.h
@@ -4,8 +4,7 @@
 #include <tipos.h>
 #include <device.h>
 
-void con_init(void);
-
+void con_init(const char* screen_saver_path);
 chardev* con_open(void);
 sint_32 con_read(chardev* this, void* buf, uint_32 size);
 sint_32 con_write(chardev* this, const void* buf, uint_32 size);
