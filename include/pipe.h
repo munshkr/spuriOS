ebfc00537d06d7e11e863f6b948c29e43c16f8af
Damián Emiliano Silvani <munshkr@gmail.com>
2011-06-21 23:52:37 -0300

    pipe: Add declarations

--- include/pipe.h
+++ include/pipe.h
@@ -4,10 +4,11 @@
 #include <tipos.h>
 #include <device.h>
 
-
 void pipe_init(void);
-
 sint_32 pipe_open(chardev* pipes[2]);
+sint_32 pipe_read(chardev* self, void* buf, uint_32 size);
+sint_32 pipe_write(chardev* self, const void* buf, uint_32 size);
+uint_32 pipe_flush(chardev* self);
 
 // Syscalls
 /*
