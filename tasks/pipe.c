afd28c1ad644f04f8cfcefe226f712089e55245c
Leandro Liptak <lliptak@dc.uba.ar>
2011-06-23 16:49:45 -0300

    Pipe task now uses fork() (and now it fails)
    Details:
    	* We must change the broken pipe handling

--- tasks/pipe.c
+++ tasks/pipe.c
@@ -14,30 +14,40 @@ int main () {
 		fprintf(con, "Pipe failed! %d\n", res);
 	} else {
 		fprintf(con, "Pipe %u for reading, %u for writing\n", pipe_fds[0], pipe_fds[1]);
+		int pid = fork();
 
-		fprintf(con, "- Write 'Hola mundo!'\n");
-		write(pipe_fds[1], "Hola mundo!", 11);
+		if (pid != 0) {
+			close(pipe_fds[0]);
+	
+			fprintf(con, "- Write 'Hola mundo!'\n");
+			write(pipe_fds[1], "Hola mundo!", 11);
 
-		fprintf(con, "- Read 5 bytes: ");
-		read(pipe_fds[0], buf, 5);
-		fprintf(con, "'%s'\n", buf);
+			fprintf(con, "- Write ' more text...'\n");
+			fprintf(pipe_fds[1], " more text...");
 
-		fprintf(con, "- Read another 5 bytes: ");
-		read(pipe_fds[0], buf, 5);
-		fprintf(con, "'%s'\n", buf);
+			fprintf(con, "done\n");
 
-		fprintf(con, "- Write ' more text...'\n");
-		fprintf(pipe_fds[1], " more text...");
+			close(pipe_fds[1]);
+		} else {
+			// Child
+			close(pipe_fds[1]);
 
-		fprintf(con, "- Read remaining 14 bytes: ");
-		read(pipe_fds[0], buf, 14);
-		fprintf(con, "'%s'\n", buf);
+			fprintf(con, "- Read 5 bytes: ");
+			read(pipe_fds[0], buf, 5);
+			fprintf(con, "'%s'\n", buf);
 
-		close(pipe_fds[0]);
-		close(pipe_fds[1]);
+			fprintf(con, "- Read another 5 bytes: ");
+			read(pipe_fds[0], buf, 5);
+			fprintf(con, "'%s'\n", buf);
+
+			fprintf(con, "- Read remaining 14 bytes: ");
+			read(pipe_fds[0], buf, 14);
+			fprintf(con, "'%s'\n", buf);
+
+			close(pipe_fds[0]);
+		}
 	}
 
-	fprintf(con, "done\n");
 	while (1) { sleep(10); };
 	
 	close(con);
