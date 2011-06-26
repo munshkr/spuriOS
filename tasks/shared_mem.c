b0ef96e116e7b44b887c218cde408515f24619ce
munshkr <munshkr@gmail.com>
2011-06-26 20:31:44 -0300

    shared-memory: Fix semaphore issues. Shared memory works!

--- tasks/shared_mem.c
+++ tasks/shared_mem.c
@@ -3,10 +3,17 @@
 #include <fs.h>
 #include <lib.h>
 
-char buf[4096] = "";
+sem_t sem_p, sem_c;
+int con;
+
+static void cleanup() {
+	sem_close(&sem_c);
+	sem_close(&sem_p);
+	close(con);
+}
 
 int main () {
-	int con = open("/console", FS_OPEN_RDWR);
+	con = open("/console", FS_OPEN_RDWR);
 
 	char* shared = palloc();
 
@@ -15,66 +22,74 @@ int main () {
 		return -1;
 	}
 
-	sem_t sema;
-	sem_open(&sema);
+	memset(shared, 0, 4096);
+
+	sem_open(&sem_p);
+	sem_open(&sem_c);
 
 	int pid = fork();
 
-	if (pid != 0) {
-
-		sprintf(shared, "Hola hijo...\n");
-
-		sem_signal(&sema, 1);
-		sem_wait(&sema, 1);
-
-		if (strcmp(shared, "Hola padre!!!\n")) {
-			fprintf(con, "Mi hijo no me escucha...");
-			sem_close(&sema);
-			close(con);
-			return -1;
-		}
-
-		fprintf(con, "Mi hijo me ha escuchado...");
-
-		sprintf(shared, "Duerme ninio\n");
-		sem_signal(&sema, 2);
-		sem_wait(&sema, 1);
-
-		sem_close(&sema);
-	} else {
-		// Child
-		sem_wait(&sema, 1);
+	if (!pid) {
+		// Child process
+		sem_wait(&sem_c, 1);
 
 		if (strcmp(shared, "Hola hijo...\n")) {
 			fprintf(con, "Mi padre no me habla!!!");
-			sem_close(&sema);
-			close(con);
+			getch(con);
+			cleanup();
 			return -1;
 		}
 
 		fprintf(con, "Mi padre me ha hablado!!!\n");
 
 		sprintf(shared, "Hola padre!!!\n");
-		sem_signal(&sema, 1);
-		sem_wait(&sema, 2);
+		sem_signal(&sem_p, 1);
+		sem_wait(&sem_c, 1);
 
 		if (strcmp(shared, "Duerme ninio\n")) {
 			fprintf(con, "Mi padre me abandona!!!");
-			sem_close(&sema);
-			close(con);
+			getch(con);
+			cleanup();
 			return -1;
 		}
 
 		fprintf(con, "Mi padre me ha dicho que duerma!!!\n");
 
 		sprintf(shared, "Adios.\n");
-		sem_signal(&sema, 1);
-		sem_close(&sema);
+		sem_signal(&sem_p, 1);
+
+		cleanup();
+		return 0;
 	}
 
-	while (1) { sleep(10); };
+	sprintf(shared, "Hola hijo...\n");
 
-	close(con);
+	sem_signal(&sem_c, 1);
+	sem_wait(&sem_p, 1);
+
+	if (strcmp(shared, "Hola padre!!!\n")) {
+		fprintf(con, "Mi hijo no me escucha...\n");
+		getch(con);
+		cleanup();
+		return -1;
+	}
+
+	fprintf(con, "Mi hijo me ha escuchado...\n");
+
+	sprintf(shared, "Duerme ninio\n");
+	sem_signal(&sem_c, 1);
+	sem_wait(&sem_p, 1);
+
+	if (strcmp(shared, "Adios.\n")) {
+		fprintf(con, "Mi hijo no me hace caso, changos\n");
+		getch(con);
+		cleanup();
+		return -1;
+	}
+
+	fprintf(con, "Press any key to finish\n");
+	getch(con);
+
+	cleanup();
 	return 0;
 }
-
